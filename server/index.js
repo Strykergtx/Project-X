6c647575143d1a24b512630dea0a81c507ea834f
Srikar Rao <32897530+Strykergtx@users.noreply.github.com>
2021-04-28 15:00:25 +0530

    schema
    added schema

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+var cors = require('cors')
+var cookieParser = require('cookie-parser')
+var session = require('express-session');
+mongoose = require('mongoose').set('debug', true);
+const uri = "mongodb+srv://spacelender:admin123@spacelendermaster.nodha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 const app = express(); // create express app
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("This is from express.js");
